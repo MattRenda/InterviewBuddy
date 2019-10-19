993345e723f0f8a5757189f8eb5f8b08ffd8c440
MattRenda <matthewrenda14@yahoo.com>
2019-10-19 13:56:31 -0700

    Updates

--- precache-manifest.194dff97d47b26e66ad6616afdbc0a45.js
+++ precache-manifest.194dff97d47b26e66ad6616afdbc0a45.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2072e68758f2fb4d3b7a26473de3e042",
+    "revision": "1bbd2ece67195561d4fc93f8b629cf10",
     "url": "/interview_buddy/index.html"
   },
   {
-    "revision": "44e6aae03e0ac3a8bec9",
+    "revision": "c57507b2ed2a8840afbc",
     "url": "/interview_buddy/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "44e6aae03e0ac3a8bec9",
-    "url": "/interview_buddy/static/js/2.340b8ebf.chunk.js"
+    "revision": "c57507b2ed2a8840afbc",
+    "url": "/interview_buddy/static/js/2.da8c7683.chunk.js"
   },
   {
-    "revision": "b27946fec7bcea06231d",
-    "url": "/interview_buddy/static/js/main.b967963a.chunk.js"
+    "revision": "6e0e20598085ef20767e",
+    "url": "/interview_buddy/static/js/main.b15432a1.chunk.js"
   },
   {
     "revision": "828c423e40df1efcb8a1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/interview_buddy/precache-manifest.ba2111814c0fca77593a25dd5a3cedf7.js"
+  "/interview_buddy/precache-manifest.194dff97d47b26e66ad6616afdbc0a45.js"
 );
 
 self.addEventListener('message', (event) => {
