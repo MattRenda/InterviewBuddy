262335e5baecbec57f75ea735d0076d59048d815
MattRenda <matthewrenda14@yahoo.com>
2019-10-19 01:24:31 -0700

    Updates

---
+++ precache-manifest.6181ce9de5d1a854365c92886d7337b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c814d0f4c3db62058c1ec9c678a91ac",
+    "url": "/interview_buddy/index.html"
+  },
+  {
+    "revision": "eed75b7b96ecddb04342",
+    "url": "/interview_buddy/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "eed75b7b96ecddb04342",
+    "url": "/interview_buddy/static/js/2.bb14ecae.chunk.js"
+  },
+  {
+    "revision": "8d932656d26b40f9e8ec",
+    "url": "/interview_buddy/static/js/main.93366652.chunk.js"
+  },
+  {
+    "revision": "828c423e40df1efcb8a1",
+    "url": "/interview_buddy/static/js/runtime-main.e99b5963.js"
+  }
+]);
