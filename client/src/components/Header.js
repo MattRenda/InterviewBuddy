387f28617bb9cdb9819a80d6985b8613492a4d0b
MattRenda <matthewrenda14@yahoo.com>
2019-12-30 21:32:06 -0800

    commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,24 +4,25 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 export const Header = () => {
-  
+
     return (
-        <div>
-            <div className='constainerStyle' >
-                <a className='linkStyle' href='/'><div className='brandStyle'>Interview Buddy</div></a>
-            </div>
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col xs lg="2">
-                    </Col>
-                    <Col md="auto">
-                        <Button className='style' variant="info" href='/'>Home</Button>
-                        <Button className='style' variant="info" href='/about'>About</Button>
-                    </Col>
-                    <Col xs lg="2">
-                    </Col>
-                </Row>
-            </Container>
+        <div className='header-color'>
+            <Row >
+                <Col >
+                    <a style={{ textDecoration: 'none' }} href='/'>
+                        <div className='linkStyle'>Interview Buddy</div>
+                    </a>
+                </Col>
+
+                <Col >
+                    <a style={{ textDecoration: 'none' }} variant="info" href='/'>
+                        <div className='pos-home'>Home</div></a>
+                    <a style={{ textDecoration: 'none' }} variant="info" href='/about'>
+                        <div className='pos-about'>About</div></a>
+                </Col>
+            </Row>
+
+
         </div>
 
     )
